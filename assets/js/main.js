a616c8c39e9235f25738563573027320303d8c50
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-28 17:02:10 +0200

    add square_but:not correct

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-/*Scrivi un programma che stampi in console i numeri da 1 a 100.
+/*
+Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100.
+
+
 MILESTONE 1
 Per i multipli di 3 stampi “Fizz” al posto del numero
 Per i multipli di 5 stampi Buzz.
@@ -7,17 +11,25 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 //scrivo il programma che stampi i numeri in console da 1 a 100.
 
-const listHtml = document.getElementById("list")
+const listHtml = document.getElementById("list");
+
+
+
 
 for ( let i = 1; i < 101; i++){
     console.log(i)
 
-    const listItem = `<li>${i}</li>`;
+    const listItem = `<li> ${i}</li>`;
     listHtml.innerHTML = listHtml.innerHTML + listItem;
-
+    
 }
     
 
 
+
+
+
+
+//creazione square
 
 
