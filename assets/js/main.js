d600387a567ea731505df53aa3380cb057174d95
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-28 15:35:12 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*Scrivi un programma che stampi in console i numeri da 1 a 100.
+MILESTONE 1
+Per i multipli di 3 stampi “Fizz” al posto del numero
+Per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+//scrivo il programma che stampi i numeri in console da 1 a 100.
+
+const listHtml = document.getElementById("list")
+
+for ( let i = 1; i < 101; i++){
+    console.log(i)
+
+    const listItem = `<li>elemento lista n° ${i}</li>`;
+
+}
+    
+
+
+
+
